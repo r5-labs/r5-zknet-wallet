ac2600a0abf77d4fb0d523c4651f1b309651e3de
ZNX <henrique.baronceli@gmail.com>
2025-03-25 23:11:25 +1300

    add consts

---
+++ r5-zknet-wallet/src/constants/index.ts
@@ -0,0 +1,9 @@
+export const mainnetExplorerUrl = 'https://explorer.r5.network/'
+export const testnetExplorerUrl = 'https://explorer.testnet.r5.network/'
+export const devnetExplorerUrl = 'https://explorer.devnet.r5.network/'
+export const mainnetChainId = '337'
+export const testnetChainId = '33710'
+export const devnetChainId = '33711'
+export const mainnetZknetAddress = '0x1A52C4914F8A0c254C69699631a1C92De4cCf01A' // not correct (devnet)
+export const testnetZknetAddress = '0x1A52C4914F8A0c254C69699631a1C92De4cCf01A' // not correct (devnet)
+export const devnetZknetAddress = '0x1A52C4914F8A0c254C69699631a1C92De4cCf01A'
