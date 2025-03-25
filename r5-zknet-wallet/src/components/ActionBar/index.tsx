86b5daf465287d7d0db7e8829e499dca04fa14b3
ZNX <henrique.baronceli@gmail.com>
2025-03-25 23:06:25 +1300

    add icons, abi, ui upd

--- r5-zknet-wallet/src/components/ActionBar/index.tsx
+++ r5-zknet-wallet/src/components/ActionBar/index.tsx
@@ -2,22 +2,40 @@ import {
   BoxContent,
   BoxContentParent,
   ButtonRound,
-  ButtonSecondary
 } from "../../theme";
+import { BsQrCode } from "react-icons/bs";
+import { LuArrowDownToLine } from "react-icons/lu";
+import { LuArrowUpToLine } from "react-icons/lu";
+import { BsTrash } from "react-icons/bs";
+import { LuArrowUpRight } from "react-icons/lu";
+
+const ReceiveIcon = BsQrCode as React.FC<React.PropsWithChildren>;
+const DepositIcon = LuArrowDownToLine as React.FC<React.PropsWithChildren>;
+const SendInternalIcon = LuArrowUpRight as React.FC<React.PropsWithChildren>;
+const SendExternalIcon = LuArrowUpToLine as React.FC<React.PropsWithChildren>;
+const DestroyIcon = BsTrash as React.FC<React.PropsWithChildren>;
 
 export function ActionBar() {
   return (
     <BoxContentParent>
       <BoxContent>
-        <ButtonRound>D</ButtonRound>
-        <ButtonRound>S</ButtonRound>
-        <ButtonRound>R</ButtonRound>
-        <ButtonRound>E</ButtonRound>
-        <ButtonRound>D</ButtonRound>
+        <ButtonRound title="Deposit Funds">
+          <DepositIcon />
+        </ButtonRound>
+        <ButtonRound title="Withdraw Funds">
+          <SendExternalIcon />
+        </ButtonRound>
+        <ButtonRound title="Destroy Account">
+          <DestroyIcon />
+        </ButtonRound>
+        <ButtonRound title="Send Transaction">
+          <SendInternalIcon />
+        </ButtonRound>
+        <ButtonRound title="Receive Transaction">
+          <ReceiveIcon />
+        </ButtonRound>
       </BoxContent>
-      <BoxContent>
-        <ButtonSecondary>Validate Hash</ButtonSecondary>
-      </BoxContent>
+      <BoxContent />
     </BoxContentParent>
   );
 }
